0d2b42124d96dd6e81adea40dedb4f54a200584c
Pragati <tanpure0011@gmail.com>
2022-07-11 20:10:12 +0530

    first commit

--- UploadFileServer/UploadFileServer/Controllers/UploadController.cs
+++ UploadFileServer/UploadFileServer/Controllers/UploadController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,17 +18,25 @@ namespace SampleWebApp.Controllers
             {
                 var formCollection = await Request.ReadFormAsync();
                 var file = formCollection.Files.First();
-                var folderName = Path.Combine("Resources", "Images");
-                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                //var folderName = Path.Combine("Resources", "Images");
+                //var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory());
                 if (file.Length > 0)
                 {
                     var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
                     var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
+                    //var dbPath = Path.Combine(folderName, fileName);
                     using (var stream = new FileStream(fullPath, FileMode.Create))
                     {
                         file.CopyTo(stream);
                     }
+                    string connectionString = "DefaultEndpointsProtocol=https;AccountName=uploadimage12;AccountKey=bz2exF4uErrsw9un91YzXBzhF2umhenkhbmzf+DRnRVG/i7tPtSE9RUiMoT9Eg8hr5Fa1NRQSO/j+AStrIH9rQ==;EndpointSuffix=core.windows.net";
+                    string containerName = "images";
+                    BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+                    var blob = container.GetBlobClient(fileName);
+                    var blobstream = System.IO.File.OpenRead(fileName);
+                    await blob.UploadAsync(blobstream);
+                    var Uri = blob.Uri.AbsoluteUri;
                     return Ok();
                 }
                 else
