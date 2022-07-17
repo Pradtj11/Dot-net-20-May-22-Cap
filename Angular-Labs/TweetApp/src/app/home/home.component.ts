59a76552e4fd7ab0f12735ab18d59462065fe292
Pragati <tanpure0011@gmail.com>
2022-07-17 18:06:11 +0530

    first commit

--- Angular-Labs/TweetApp/src/app/home/home.component.ts
+++ Angular-Labs/TweetApp/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserData } from '../models/UserData';
 import { AccountService } from '../services/account.service';
 import { AuthService } from '../services/auth.service';
+import { TweetService } from '../services/tweet.service';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,14 @@ export class HomeComponent implements OnInit {
   public searchTerm: string='';
     
   AuthService: any;
+  tweets: any;
 
-  constructor(private auth: AuthService, private accountService :AccountService) { }
+  constructor(private auth: AuthService, private accountService :AccountService, private _tweetservice: TweetService) { }
 
   users: Array<UserData> = new Array<UserData>();
   ngOnInit(): void {
 
+    this._tweetservice.getProducts().subscribe(res => this.tweets = res, err => console.log(err))
     this.auth.getUser().subscribe((res: UserData[]) => this.users = res, (err: any) => console.log(err))
   }
   search(event:any){
